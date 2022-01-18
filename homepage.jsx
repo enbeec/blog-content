d2890d8291bc0b6ceaeb676f4cf002115b5ec4ad
Val Currie <contact@va.lcuri.email>
2022-01-18 00:56:53 -0600

    content init

---
+++ homepage.jsx
@@ -0,0 +1,16 @@
+export const avatar = {
+  url: `https://github.com/enbeec.png`,
+  alt: `A picture of Val Currie looking down with a natural stone arch behind her. She is wearing sunglasses, a black baseball cap and a black shirt with the sleeves cut off.`
+};
+
+export const description = (
+  <>
+    {`This is my place to publicly document what I'm up to and what I learn along the way. 
+				I'm going to experiment a lot with structured documents and custom rendering for 
+				patterns that emerge while I document my thoughts over time. Please open an issue on `}
+    <a href="https://github.com/enbeec/blog.valcurrie.com" target="_blank" rel="noreferrer">
+      GitHub
+    </a>
+    {` if you notice something wrong or have an idea! I'd love to hear from you.`}
+  </>
+);
